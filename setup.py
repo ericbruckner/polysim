ee8c32ed8b0b221f7a71cf6b7bc3d6450a69f656
ericbruckner <70349922+ericbruckner@users.noreply.github.com>
2021-02-22 15:41:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
-from setuptools import setup, find_packages
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from distutils.core import setup
 
 setup(
     name="poly_dpd", 
     version="0.1.0",
+    packages = ['poly_dpd'],
     author="Eric Bruckner",
     author_email="eric.p.bruckner@gmail.com",
     description="A library for running DPD simulations of simple polymers with HOOMD-blue",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(include = 'poly_dpd'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"
-    ],
-    python_requires='>=3',
 )
